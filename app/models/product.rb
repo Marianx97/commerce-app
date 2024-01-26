2570b541453a6c89bc5c4d57b8f66e8c649f5fc7
Mariano Ranucci <ranucci.mariano@gmail.com>
2024-01-26 10:37:01 -0300

    Added active_storage attachments to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   validates :title, presence: :true
   validates :description, presence: :true
   validates :price, presence: :true
+
+  has_one_attached :photo
 end
